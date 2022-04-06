bdd44c4b1a3a87b11486ff543955f6476027df58
TomasNikolov <tomasnik81@gmail.com>
2022-04-06 14:30:08 +0300

    Changes in ExpenseIt project

--- ExpenseIt/ExpenseItHome.xaml.cs
+++ ExpenseIt/ExpenseItHome.xaml.cs
@@ -17,19 +17,100 @@ namespace ExpenseIt
     public partial class ExpenseItHome : Window
     {
         public string MainCaptionText { get; set; }
+        public List<Person> ExpenseDataSource { get; set; }
+        public DateTime LastChecked { get; set; }
 
         public ExpenseItHome()
         {
             InitializeComponent();
+            LastChecked = DateTime.Now;
+            this.DataContext = this;
+
+            ExpenseDataSource = new List<Person>()
+            {
+                new Person()
+                {
+                    Name="Mike",
+                    Department ="Legal",
+                    Expenses = new List<Expense>()
+                    {
+                        new Expense() { ExpenseType="Lunch", ExpenseAmount =50 },
+                        new Expense() { ExpenseType="Transportation", ExpenseAmount=50 }
+                    }
+                },
+                new Person()
+                {
+                    Name ="Lisa",
+                    Department ="Marketing",
+                    Expenses = new List<Expense>()
+                    {
+                        new Expense() { ExpenseType="Document printing", ExpenseAmount=50 },
+                        new Expense() { ExpenseType="Gift", ExpenseAmount=125 }
+                    }
+                },
+                new Person()
+                {
+                    Name="John",
+                    Department ="Engineering",
+                    Expenses = new List<Expense>()
+                    {
+                        new Expense() { ExpenseType="Magazine subscription", ExpenseAmount=50 },
+                        new Expense() { ExpenseType="New machine", ExpenseAmount=600 },
+                        new Expense() { ExpenseType="Software", ExpenseAmount=500 }
+                    }
+                },
+                new Person()
+                {
+                    Name="Mary",
+                    Department ="Finance",
+                    Expenses = new List<Expense>()
+                    {
+                        new Expense() { ExpenseType="Dinner", ExpenseAmount=100 }
+                    }
+                },
+                new Person()
+                {
+                    Name="Theo",
+                    Department ="Marketing",
+                    Expenses = new List<Expense>()
+                    {
+                        new Expense() { ExpenseType="Dinner", ExpenseAmount=100 }
+                    }
+                },
+                new Person()
+                {
+                    Name="James",
+                    Department ="Engineering",
+                    Expenses = new List<Expense>()
+                    {
+                        new Expense() { ExpenseType="Magazine subscription", ExpenseAmount=50 },
+                        new Expense() { ExpenseType="New machine", ExpenseAmount=600 },
+                        new Expense() { ExpenseType="Software", ExpenseAmount=500 }
+                    }
+                },
+                new Person()
+                {
+                    Name="David",
+                    Department ="Finance",
+                    Expenses = new List<Expense>()
+                    {
+                        new Expense() { ExpenseType="Dinner", ExpenseAmount=100 }
+                    }
+                }
+            };
+
             MainCaptionText = "View Expense Report :";
         }
 
         private void viewBtn_Click(object sender, RoutedEventArgs e)
         {
-            ExpenseReport report = new ExpenseReport();
-            report.Height = this.Height;
-            report.Width = this.Width;
-            report.ShowDialog();
+            ExpenseReport expenseReport = new ExpenseReport(this.peopleListBox.SelectedItem)
+            {
+                Height = this.Height,
+                Width = this.Width
+            };
+            expenseReport.ShowDialog();
+            this.Close();
         }
     }
 }
--- ExpenseIt/ExpenseReport.xaml.cs
+++ ExpenseIt/ExpenseReport.xaml.cs
@@ -14,14 +14,16 @@ using System.Windows.Shapes;
 
 namespace ExpenseIt
 {
-    /// <summary>
-    /// Interaction logic for ExpenseReport.xaml
-    /// </summary>
     public partial class ExpenseReport : Window
     {
         public ExpenseReport()
         {
             InitializeComponent();
+        }
+
+        public ExpenseReport(object data) : this()
+        {
+            this.DataContext = data;
         }
     }
 }
